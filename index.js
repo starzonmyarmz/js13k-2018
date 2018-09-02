8c37e6a6d52e8761e348d5be9a31ad799aedcca3
Brad Dunbar <dunbarb2@gmail.com>
2018-09-02 09:03:06 -0400

    sound module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Body from './src/body.js'
+import {GOAL_FX, JUMP_FX, DEATH_FX} from './src/sound.js'
 
 const svg = document.querySelector('svg')
-const ac = new AudioContext()
 const KEYS = {}
 const WIDTH = 768
 const HEIGHT = 480
@@ -13,44 +13,6 @@ const NO_DEFAULT = [
   'ArrowRight'
 ]
 
-const JUMP_FX = new TinyMusic.Sequence(ac, 180, [
-  'C4 e',
-  'G5 e',
-  'C5 e'
-]);
-
-const GOAL_FX = new TinyMusic.Sequence(ac, 280, [
-  'G3 0.125',
-  'Ab3 0.125',
-  'A3 0.125',
-  'Bb3 0.125',
-  'B3 0.125',
-  'C4 0.125',
-  'Db4 0.125',
-  'D4 0.125',
-  'Eb4 0.125',
-  'E4 0.125',
-  'F4 0.125',
-  'Gb4 0.125',
-  'G4 w'
-]);
-
-const DEATH_FX = new TinyMusic.Sequence(ac, 180, [
-  'E2 e',
-  'E1 q'
-]);
-
-JUMP_FX.loop = false
-JUMP_FX.smoothing = 1
-
-GOAL_FX.loop = false
-GOAL_FX.smoothing = 0.2
-GOAL_FX.staccato = 0.1
-
-DEATH_FX.loop = false
-DEATH_FX.waveType = 'sawtooth'
-DEATH_FX.bass.gain.value = 10;
-DEATH_FX.smoothing = 0.5
 
 const sleep = (delay) => new Promise((resolve, reject) => {
   let start = performance.now()
