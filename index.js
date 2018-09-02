538a385da3cd543ddb5e2f41683ae5d384ea97f0
Brad Dunbar <dunbarb2@gmail.com>
2018-09-02 08:55:38 -0400

    modules

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Body from './src/body.js'
+
 const svg = document.querySelector('svg')
 const ac = new AudioContext()
 const KEYS = {}
@@ -67,72 +69,6 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('keyup', (event) => {
   KEYS[event.key] = false
 })
-
-class Body {
-  constructor (element) {
-    this.element = element
-    this.bounds = {}
-  }
-
-  get x () {
-    return this._x
-  }
-
-  set x (value) {
-    this.element.setAttribute('x', this._x = value || 0)
-  }
-
-  get y () {
-    return this._y
-  }
-
-  set y (value) {
-    this.element.setAttribute('y', this._y = value || 0)
-  }
-
-  get top () {
-    return this.y
-  }
-
-  get bottom () {
-    return this.y + this.height
-  }
-
-  get left () {
-    return this.x
-  }
-
-  get right () {
-    return this.x + this.width
-  }
-
-  set bottom (value) {
-    this.y = value - this.height
-  }
-
-  isLeftOf (other) {
-    return this.right <= other.left
-  }
-
-  isRightOf (other) {
-    return this.left >= other.right
-  }
-
-  isAbove (other) {
-    return this.bottom <= other.top
-  }
-
-  isBelow (other) {
-    return this.top >= other.bottom
-  }
-
-  overlaps (other) {
-    return this.left < other.right &&
-    this.right > other.left &&
-    this.top < other.bottom &&
-    this.bottom > other.top
-  }
-}
 
 class Goal extends Body {
   constructor () {
